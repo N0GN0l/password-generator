65773519459faa13d74595eb043c0a6302e4ddb9
Daniel Zhou <danielzhou323@gmail.com>
2023-11-11 23:18:13 -0500

    reduced clutter through the use of seperate method, also everything turned static

--- Main.java
+++ Main.java
@@ -2,43 +2,40 @@ import java.lang.Math;
 import java.util.Scanner;
 
 class Main {
+    static String password = "";
+    static String[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+    static String[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+            "r",
+            "s", "t", "u", "v", "w", "x", "y", "z" };
+    static String[] symbols = { "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[",
+            "{",
+            "]", "}", "\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?" };
+
     public static void main(String[] args) {
-        String password = "";
-        String[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-        String[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
-                "s", "t", "u", "v", "w", "x", "y", "z" };
-        String[] symbols = { "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{",
-                "]", "}", "\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?" };
+
         try (Scanner myObj = new Scanner(System.in)) {
             String passwordName;
             // Enter username and press Enter
             System.out.println("Enter website");
             passwordName = myObj.nextLine();
             for (int i = 0; i <= ((int) (Math.random() * 15) + 3); i++) {
-                int tempNum = (int) (Math.random() * 10);
                 // System.out.println(tempNum);
-                if (tempNum % 2 == 0) {
-                    password += symbols[(int) (Math.random() * symbols.length)];
-                    password += symbols[(int) (Math.random() * symbols.length)];
-                } else {
-                    password += symbols[(int) (Math.random() * symbols.length)];
-                }
-                if (tempNum % 2 == 0) {
-                    password += numbers[(int) (Math.random() * numbers.length)];
-                    password += numbers[(int) (Math.random() * numbers.length)];
-                } else {
-                    password += numbers[(int) (Math.random() * numbers.length)];
-                }
-                if (tempNum % 2 == 0) {
-                    password += letters[(int) (Math.random() * letters.length)];
-                    password += letters[(int) (Math.random() * letters.length)];
-                } else {
-                    password += letters[(int) (Math.random() * letters.length)].toUpperCase();
-                }
-
+                randomValue(numbers);
+                randomValue(letters);
+                randomValue(symbols);
             }
             System.out.print(passwordName + " password: ");
         }
         System.out.println(password);
+    }
+
+    public static void randomValue(String[] array) {
+        int tempNum = (int) (Math.random() * 10);
+        if (tempNum % 2 == 0) {
+            password += array[(int) (Math.random() * array.length)];
+            password += array[(int) (Math.random() * array.length)];
+        } else {
+            password += array[(int) (Math.random() * array.length)];
+        }
     }
 }
